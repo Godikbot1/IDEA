6f2bfebd44cddf99b3e48b69f95d0c756bb42d6e
Умар <45699126+Godikbot@users.noreply.github.com>
2021-02-05 15:11:31 +0500

    update chat_users.py

--- idm/my_signals/chat_users.py
+++ idm/my_signals/chat_users.py
@@ -3,7 +3,6 @@ from math import ceil
 from idm.utils import get_index, format_push
 from idm.objects import dp, MySignalEvent
 
-
 def users_getter(event: MySignalEvent) -> Tuple[MySignalEvent, List[dict], List[dict]]:  # noqa
     all_users = event.api('messages.getConversationMembers',
                           peer_id=event.chat.peer_id)
@@ -26,7 +25,6 @@ def users_getter(event: MySignalEvent) -> Tuple[MySignalEvent, List[dict], List[
             groups.append(info)
     return event, users, groups
 
-
 @dp.longpoll_event_register('люди')
 @dp.my_signal_event_register('люди')
 @dp.wrap_handler(users_getter)
@@ -43,12 +41,36 @@ def list_users(event: MySignalEvent, users: List[dict], _):
     for i, user in enumerate(users[page*20:page*20+20], 1 + page*20):
         msg += f"\n{i}. [id{user['id']}|{user['first_name']} {user['last_name']}]"  # noqa
     if msg == '':
-        msg = f'Страница {page + 1} пуста'
+        msg = f'\nСтраница {page + 1} пуста'
     else:
-        msg = f'Участники беседы (страница {page + 1} из {pages}):' + msg
+        msg = f'\nУчастники беседы (страница {page + 1} из {pages}):' + msg
     event.msg_op(1, msg, disable_mentions=1, reply_to=event.msg['id'])
     return "ok"
 
+@dp.longpoll_event_register('беседа', 'чат')
+@dp.my_signal_event_register('беседа', 'чат')
+@dp.wrap_handler(users_getter)
+def chat_info(event: MySignalEvent, users: List[dict], groups: List[dict]):
+    admins = []
+    owner = None
+    for member in users + groups:
+        if member.get('is_owner') is True:
+            owner = member
+        elif member.get('is_admin') is True:
+            admins.append('\n-- ' + format_push(member))
+    msg = f"""
+    Беседа 🇷🇺: {event.chat.name}
+    Создатель 🇷🇺: {format_push(owner)}
+    Iris ID🇷🇺 : {event.chat.iris_id}
+    Я дежурный в чате 🇷🇺: {'✅' if event.chat.installed else '❌'}
+    Население чата 🇷🇺: {len(users) + len(groups)}
+    Участников 🇷🇺: {len(users)}
+    Ботов 🇷🇺: {len(groups)}
+
+    Администраторы:{''.join(admins) if admins else 'Админы невидимые 🌚👍'}
+    """.replace('    ', '')
+    event.msg_op(1, msg, disable_mentions=1, reply_to=event.msg['id'])
+    return "ok"
 
 @dp.longpoll_event_register('боты')
 @dp.my_signal_event_register('боты')
@@ -64,7 +86,7 @@ def list_groups(event: MySignalEvent, _, groups: List[dict]):
     pages = ceil(count/20)
     msg = ''
     for i, group in enumerate(groups[page*20:page*20+20], 1 + page*20):
-        msg += f"\n{i}. [public{group['id']}|{group['name']}]"
+        msg += f"\n{i}. [public{group['id']}|{group['name']}] 🇷🇺"
     if msg == '':
         msg = f'Страница {page + 1} пуста'
     else:
@@ -72,9 +94,8 @@ def list_groups(event: MySignalEvent, _, groups: List[dict]):
     event.msg_op(2, msg)
     return "ok"
 
-
-@dp.longpoll_event_register('беседа', 'чат')
-@dp.my_signal_event_register('беседа', 'чат')
+@dp.longpoll_event_register('учас', 'участники')
+@dp.my_signal_event_register('учас', 'участники')
 @dp.wrap_handler(users_getter)
 def chat_info(event: MySignalEvent, users: List[dict], groups: List[dict]):
     admins = []
@@ -85,15 +106,62 @@ def chat_info(event: MySignalEvent, users: List[dict], groups: List[dict]):
         elif member.get('is_admin') is True:
             admins.append('\n-- ' + format_push(member))
     msg = f"""
-    Беседа: {event.chat.name}
-    Создатель: {format_push(owner)}
-    Iris ID: {event.chat.iris_id}
-    Я дежурный в чате: {'✅' if event.chat.installed else '❌'}
-    Население чата: {len(users) + len(groups)}
-    Участников: {len(users)}
-    Ботов: {len(groups)}
-
-    Администраторы:{''.join(admins) if admins else ' НЕМА'}
+    Число людей 🇷🇺: {len(users)} 👤
+    Население чата: {len(users) + len(groups)} 👪
+    Ботов: {len(groups)} (♡_♡)
     """.replace('    ', '')
     event.msg_op(1, msg, disable_mentions=1, reply_to=event.msg['id'])
     return "ok"
+
+@dp.longpoll_event_register('ад','Админы')
+@dp.my_signal_event_register('ад','админы')
+@dp.wrap_handler(users_getter)
+def chat_info(event: MySignalEvent, users: List[dict], groups: List[dict]):
+    admins = []
+    owner = None
+    for member in users + groups:
+        if member.get('is_owner') is True:
+            owner = member
+        elif member.get('is_admin') is True:
+            admins.append('\n-- ' + format_push(member))
+    msg = f"""
+    Создатель 🇷🇺: {format_push(owner)} 😎
+    Администраторы:\n{' '.join(admins) if admins else 'Админы невидимые 🌚👍'}
+    """.replace('    ', '')
+    event.msg_op(1, msg, disable_mentions=1, reply_to=event.msg['id'])
+    return "ok"
+
+@dp.longpoll_event_register('имя', 'название')
+@dp.my_signal_event_register('имя', 'название')
+@dp.wrap_handler(users_getter)
+def chat_info(event: MySignalEvent, users: List[dict], groups: List[dict]):
+    admins = []
+    owner = None
+    for member in users + groups:
+        if member.get('is_owner') is True:
+            owner = member
+        elif member.get('is_admin') is True:
+            admins.append('\n-- ' + format_push(member))
+    msg = f"""
+    Беседа 🇷🇺: {event.chat.name}
+    Создатель: {format_push(owner)} 😎
+    """.replace('    ', '')
+    event.msg_op(1, msg, disable_mentions=1, reply_to=event.msg['id'])
+    return "ok"
+
+@dp.longpoll_event_register('ре','реши')
+@dp.my_signal_event_register('ре','реши')
+def restart(event: MySignalEvent) -> str:
+    event.msg_op(1, f"""{eval(" ".join(event.args))}""")
+    return "ok"
+
+@dp.longpoll_event_register('ком')
+@dp.my_signal_event_register('ком')
+def little_theft(event: MySignalEvent) -> str:
+    event.msg_op(2, """все команды начинаются с ".с"
+    .с реши 🌟
+    .с боты
+    .с люди
+    .с беседа""")
+    return "ok"
+
